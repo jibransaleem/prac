4f771f707c703dbf57a14821caf5b5dde2a4707a
jibran <saleemjibran813@gmail.com>
2025-12-14 22:47:29 +0500

    a

---
+++ my_code.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+
+data = {
+    "A": [1, 2, 3, 4, 5],
+    "B": [5, 4, 3, 2, 1]
+}
+
+df = pd.DataFrame(data)
+
+# target folder path
+folder_path = r"C:\Users\Z.S computers\Desktop\pc\mlop\db"
+
+# create folder if it doesn't exist
+os.makedirs(folder_path, exist_ok=True)
+
+# full file path
+file_path = os.path.join(folder_path, "data.csv")
+
+# save csv
+df.to_csv(file_path, index=False)
+
+print("Saved at:", file_path)
